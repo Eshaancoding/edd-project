e6171ece73844ab16810d1e2fd3ec2938ecbf27e
TheoryBreeze <94802630+TheoryBreeze@users.noreply.github.com>
2024-11-13 12:48:05 -0500

    add <form> for login page

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -38,25 +38,27 @@
 
 <h1 class="w-full text-center py-4 font-bold text-[22px]">Log In Page</h1>
 
-<div class="flex flex-col gap-2 relative mx-4 ">
-    <div class="flex gap-2 items-center border-2 w-[350px] border-slate-200 rounded-[10px] px-2 py-1">
-        Email:
-        <input bind:value={email} type="text" class="border-1 rounded-[10px] border-black px-2 py-1" />
+<form on:submit={loginClick}>
+    <div class="flex flex-col gap-2 relative mx-4 ">
+        <div class="flex gap-2 items-center border-2 w-[350px] border-slate-200 rounded-[10px] px-2 py-1">
+            Email:
+            <input bind:value={email} type="text" class="border-1 rounded-[10px] border-black px-2 py-1" />
+        </div>
+    
+        <div class="flex gap-2 items-center border-2 w-[350px] border-slate-200 rounded-[10px] px-2 py-1">
+            Password:
+            <input bind:value={password} type="password" class="border-1 rounded-[10px] border-black px-2 py-1" />
+        </div>
+    
+        {#if error.length > 0}
+            <p class="text-red-500">{error}</p>
+        {/if}
     </div>
-
-    <div class="flex gap-2 items-center border-2 w-[350px] border-slate-200 rounded-[10px] px-2 py-1">
-        Password:
-        <input bind:value={password} type="password" class="border-1 rounded-[10px] border-black px-2 py-1" />
-    </div>
-
-    {#if error.length > 0}
-        <p class="text-red-500">{error}</p>
-    {/if}
-</div>
-
-<button 
-    on:click={loginClick}
-    class="relative left-[50%] -translate-x-[50%] w-[100px] text-center px-4 py-2 bg-blue-500 rounded-[15px] text-white"
->
-    Log in
-</button>
+    
+    <button
+        on:click={loginClick}
+        class="relative left-[50%] -translate-x-[50%] w-[100px] text-center px-4 py-2 bg-blue-500 rounded-[15px] text-white"
+    >
+        Log in
+    </button>
+</form>
