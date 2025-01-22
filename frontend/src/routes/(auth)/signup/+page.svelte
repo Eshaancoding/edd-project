f36cfc00f4cd3ce1fd4744c790595722e3120085
TheoryBreeze <94802630+TheoryBreeze@users.noreply.github.com>
2025-01-21 20:13:38 -0500

    style: flex sign up

--- frontend/src/routes/(auth)/signup/+page.svelte
+++ frontend/src/routes/(auth)/signup/+page.svelte
@@ -30,41 +30,42 @@
     }
 </style>
 
-<div class="flex bg-orange-50 min-h-screen pt-[200px] pb-8">
-    <div class="p-5 basis-2/5">
-        <div class="bg-clouds h-full rounded-3xl p-14 flex flex-col justify-between">
-            <div class="text-5xl font-light text-white text-right leading-tight">Make the first step to break out of your comfort zone!</div>
+<div class="bg-orange-50 min-h-screen pt-[100px] pb-8">
+    <div class="container flex">
+        <div class="pl-5 basis-2/5">
+            <div class="bg-clouds h-full rounded-3xl p-14 flex flex-col justify-between">
+                <div class="text-5xl font-light text-white text-right leading-tight">Make the first step to break out of your comfort zone!</div>
+            </div>
         </div>
-    </div>
-
-    <div class="basis-3/5 px-5 pt-20">
-        <form class="flex flex-col gap-2 max-w-[400px] mx-auto" on:submit={signUpClick}>
-            <h1 class="text-5xl font-bold mb-4">Sign up</h1>
-            <div class="form-group">
-                <input type="text" class="outlined w-full" bind:value={email} required>
-                <label>Email</label>
-            </div>
-            <div class="form-group">
-                <input type="password" class="outlined" bind:value={password} required>
-                <label>Password</label>
-            </div>
-            <div class="form-group">
-                <input type="text" class="outlined w-full" bind:value={name} required>
-                <label>Name</label>
-            </div>
-            <div class="form-group">
-                <textarea class="outlined" rows="3" bind:value={bio} required></textarea>
-                <label>Bio</label>
-            </div>
-            {#each {length: 5} as _, i}
+        <div class="basis-3/5 px-5 pt-4">
+            <form class="flex flex-col gap-2 max-w-[400px] mx-auto" on:submit={signUpClick}>
+                <h1 class="text-5xl font-bold mb-4">Sign up</h1>
                 <div class="form-group">
-                    <input type="text" class="outlined" bind:value={interests[i]} required>
-                    <label>Interest {i+1}</label>
+                    <input type="text" class="outlined w-full" bind:value={email} required>
+                    <label>Email</label>
                 </div>
-            {/each}
+                <div class="form-group">
+                    <input type="password" class="outlined" bind:value={password} required>
+                    <label>Password</label>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="outlined w-full" bind:value={name} required>
+                    <label>Name</label>
+                </div>
+                <div class="form-group">
+                    <textarea class="outlined" rows="3" bind:value={bio} required></textarea>
+                    <label>Bio</label>
+                </div>
+                {#each {length: 5} as _, i}
+                    <div class="form-group">
+                        <input type="text" class="outlined" bind:value={interests[i]} required>
+                        <label>Interest {i+1}</label>
+                    </div>
+                {/each}
         
-            <button type="submit" class="bg-black hover:bg-gray-700 transition-colors text-white rounded-lg px-3 py-2">Sign up</button>
-        </form>
+                <button type="submit" class="bg-black hover:bg-gray-700 transition-colors text-white rounded-lg px-3 py-2">Sign up</button>
+            </form>
+        </div>
     </div>
 </div>
 <!-- 
